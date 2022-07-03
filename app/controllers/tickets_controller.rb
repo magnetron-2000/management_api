3b9a3c2dd59bf94efb063d3376459afe4746aeba
magnetron-2000 <danylokulba@gmail.com>
2022-07-03 20:53:25 +0300

    tickets controllers and routers

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,36 @@
+class TicketsController < ApplicationController
+
+  def index
+    render json: Ticket.all
+  end
+
+  def create
+    ticket = Ticket.new(ticket_params)
+    if ticket.save
+      render json: ticket, status: :created
+    else
+      render json: ticket.errors, status: :expectation_failed
+    end
+  end
+
+  def show
+    render json: Ticket.find(params[:id])
+  end
+
+  def update
+    ticket = Ticket.find(params[:id])
+    ticket.update(title: params[:title], description: params[:description])
+  end
+
+  def destroy
+    ticket = Ticket.find(params[:id])
+    ticket.destroy
+    render json: "#{ticket.title} deleted"
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:title, :worker_id, :description, :state)
+  end
+end
