427949ef7b6bb38e2ad747ab2a8c857aa188c4b7
magnetron-2000 <danylokulba@gmail.com>
2022-07-29 12:51:06 +0300

    change is admin in user from default nil to default false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_28_115623) do
+ActiveRecord::Schema.define(version: 2022_07_29_094727) do
 
   create_table "jwt_denylist", force: :cascade do |t|
     t.string "jti", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2022_07_28_115623) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_admin"
+    t.boolean "is_admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
