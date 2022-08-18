105517d5595b030469ff28a8352a584d597f1422
magnetron-2000 <danylokulba@gmail.com>
2022-08-18 16:44:21 +0300

    fix session test

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -5,20 +5,28 @@ RSpec.describe '/session controllers' do
     context 'post request sign in' do
       let!(:user) {create(:user)}
       let(:params_in) {{ "user": {
-        "email": "person1@mail.com",
+        "email": "person@mail.com",
         "password": "secret",
         "password_confirmation": "secret" }}}
-
-
-      it 'should return valid message if user sign in' do
-        post '/users/sign_in', :params => params_in
-        expect(JSON.parse(response.body)["message"]).to eq("You are logged in.")
-      end
-
 
       it 'should return 200 http status code if user sign in' do
         post '/users/sign_in', :params => params_in
         expect(response).to have_http_status(:ok)
+      end
+
+      it 'should return valid message if user sign in' do
+        post '/users', :params => { "user": {
+          "email": "person@mail.com",
+          "password": "secret",
+          "password_confirmation": "secret",
+          "worker_attributes": {
+            "first_name": "hi",
+            "last_name": "Bradi",
+            "age": 30,
+            "role": "Developer" } } }
+        delete '/users/sign_out'
+        post '/users/sign_in', :params => params_in
+        expect(JSON.parse(response.body)["message"]).to eq("You are logged in.")
       end
     end
   end
